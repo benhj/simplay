3e583cef865375adece65e8ba46540d1213234ba
Ben Jones <bhj.research@gmail.com>
2017-04-15 18:38:48 +0100

    arsing around with open gl and glut

--- graphics/src/glut.cpp
+++ graphics/src/glut.cpp
@@ -14,11 +14,34 @@
 #include <thread>
 #include <unistd.h>
 
+int windowWidth = 512;
+int windowHeight = 512;
+double viewDistance = 0.015;
+
 std::thread testThread;
 
 int blocks = 8;
 int popSize = 1;
 simulator::AnimatWorld animatWorld(1,{blocks, 0.3,0.5});
+
+void setScene()
+{
+    glViewport(0, 0, windowWidth, windowHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho((-windowWidth/2)*viewDistance, 
+            (windowWidth/2)*viewDistance, 
+            (-windowHeight/2 + (windowHeight/3))*viewDistance, 
+            (windowHeight/2 + (windowHeight/3))*viewDistance, 
+            -10000, 10000);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    /*
+    glRotatef(angleX,1.0,0.0,0.0);
+    glRotatef(angleY,0.0,1.0,0.0);
+    glRotatef(angleZ,0.0,0.0,1.0);
+    */
+}
 
 // This is just an example using basic glut functionality.
 // If you want specific Apple functionality, look up AGL
@@ -26,8 +49,15 @@ simulator::AnimatWorld animatWorld(1,{blocks, 0.3,0.5});
 void init() // Called before main loop to set up the program
 {
     glClearColor(0.0, 0.0, 0.0, 0.0);
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glEnable(GL_DEPTH);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_POINT_SMOOTH);
+    glEnable(GL_POLYGON_SMOOTH);
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
     glShadeModel(GL_SMOOTH);
+    setScene();
 }
 
 // Called at the start of the program, after a glutPostRedisplay() and during idle
@@ -35,6 +65,7 @@ void init() // Called before main loop to set up the program
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    setScene();
     glLoadIdentity();
     for (int p = 0; p < popSize; ++p) {
         graphics::GLAnimat(animatWorld.animat(p)).draw();
@@ -45,12 +76,7 @@ void display()
 // Called every time a window is resized to resize the projection matrix
 void reshape(int w, int h)
 {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-0.1, 0.1, -float(h)/(10.0*float(w)), float(h)/(10.0*float(w)), 0.2, 1000.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    setScene();
 }
 
 void loop()
@@ -88,10 +114,10 @@ int main(int argc, char **argv)
     glutInitWindowPosition(0, 0);
 
     // Creates a window using internal glut functionality
-    glutCreateWindow("Hello!");
+    glutCreateWindow("Animat test!");
 
     // passes reshape and display functions to the OpenGL machine for callback
-    glutReshapeFunc(reshape);
+    //glutReshapeFunc(reshape);
     glutDisplayFunc(display);
     glutIdleFunc(display);
     init();
