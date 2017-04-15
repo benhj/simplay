ff43445dcc7db4105f1ff1d029bec2422dee2587
Ben Jones <bhj.research@gmail.com>
2017-04-15 18:39:21 +0100

    arsing around with open gl and glut

--- graphics/include/GLAnimat.hpp
+++ graphics/include/GLAnimat.hpp
@@ -18,7 +18,7 @@ namespace graphics {
                 float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle 
                 float x = r * cosf(theta);//calculate the x component 
                 float y = r * sinf(theta);//calculate the y component 
-                glVertex3f(x + cx, y + cy, -10.0);//output vertex 
+                glVertex3f(x + cx, y + cy, 0);//output vertex 
             }
             glEnd();
         }
@@ -57,14 +57,14 @@ namespace graphics {
                 auto layer2Right = layer2.getPositionRight(physicsEngine);
 
                 glBegin(GL_LINES);
-                    glVertex3f(layer1Left.m_vec[0], layer1Left.m_vec[1], -10.0);
-                    glVertex3f(layer1Right.m_vec[0], layer1Right.m_vec[1], -10.0);
-                    glVertex3f(layer2Left.m_vec[0], layer2Left.m_vec[1], -10.0);
-                    glVertex3f(layer2Right.m_vec[0], layer2Right.m_vec[1], -10.0);
-                    glVertex3f(layer1Left.m_vec[0], layer1Left.m_vec[1], -10.0);
-                    glVertex3f(layer2Left.m_vec[0], layer2Left.m_vec[1], -10.0);
-                    glVertex3f(layer1Right.m_vec[0], layer1Right.m_vec[1], -10.0);
-                    glVertex3f(layer2Right.m_vec[0], layer2Right.m_vec[1], -10.0);
+                    glVertex3f(layer1Left.m_vec[0], layer1Left.m_vec[1], 0);
+                    glVertex3f(layer1Right.m_vec[0], layer1Right.m_vec[1], 0);
+                    glVertex3f(layer2Left.m_vec[0], layer2Left.m_vec[1], 0);
+                    glVertex3f(layer2Right.m_vec[0], layer2Right.m_vec[1], 0);
+                    glVertex3f(layer1Left.m_vec[0], layer1Left.m_vec[1], 0);
+                    glVertex3f(layer2Left.m_vec[0], layer2Left.m_vec[1], 0);
+                    glVertex3f(layer1Right.m_vec[0], layer1Right.m_vec[1], 0);
+                    glVertex3f(layer2Right.m_vec[0], layer2Right.m_vec[1], 0);
                 glEnd();
             }
         }
@@ -80,10 +80,10 @@ namespace graphics {
             auto layer1Left = layer1.getPositionLeft(physicsEngine);
             auto layer1Right = layer1.getPositionRight(physicsEngine);
             glBegin(GL_LINES);
-                glVertex3f(layer1Left.m_vec[0], layer1Left.m_vec[1], -10.0);
-                glVertex3f(leftAnt.m_vec[0], leftAnt.m_vec[1], -10.0);
-                glVertex3f(layer1Right.m_vec[0], layer1Right.m_vec[1], -10.0);
-                glVertex3f(rightAnt.m_vec[0], rightAnt.m_vec[1], -10.0);
+                glVertex3f(layer1Left.m_vec[0], layer1Left.m_vec[1], 0);
+                glVertex3f(leftAnt.m_vec[0], leftAnt.m_vec[1], 0);
+                glVertex3f(layer1Right.m_vec[0], layer1Right.m_vec[1], 0);
+                glVertex3f(rightAnt.m_vec[0], rightAnt.m_vec[1], 0);
             glEnd();
 
             // Draw 'bobbles' on the end of each antenna
