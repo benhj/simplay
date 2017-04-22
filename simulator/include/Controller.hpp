caf1be0fb02ff2492b206490672561afbc9cc516
Ben Jones <bhj.research@gmail.com>
2017-04-22 18:16:38 +0100

    starting to implement control mechanism

---
+++ simulator/include/Controller.hpp
@@ -0,0 +1,14 @@
+/// Copyright (c) 2017 Ben Jones
+
+#pragma once
+
+namespace simulator {
+	/// Defines a control mechanism for the animat
+	/// Will probably implement a neural version of this
+	class Controller
+	{
+	  public:
+	    virtual double getLeftMotorOutput(int const i) const = 0;
+	    virtual double getRightMotorOutput(int const i) const = 0;
+	};
+}
